28fa86e3031a26473a01769235a05370c8f357bb
7022Andre <7022Andre@users.noreply.github.com>
2017-06-08 15:01:15 -0700

    Change smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
-    domain: "gmail.com",
+    domain: "https://hidden-cliffs-95626.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
   }
 end
 

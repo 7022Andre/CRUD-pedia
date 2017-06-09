ce8f40e4f73ddbea6c6cd7b85ead22749fcec64e
7022Andre <7022Andre@users.noreply.github.com>
2017-06-09 09:13:03 -0700

    Change SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,7 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    enable_starttls_auto: true
+    password: ENV["GMAIL_PASSWORD"]
   }
 end
 

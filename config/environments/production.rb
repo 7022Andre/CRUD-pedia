2151f4a1adc138a0c38e6c72775df9e75e5a8ef8
7022Andre <7022Andre@users.noreply.github.com>
2017-06-09 09:12:01 -0700

    Change SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/hidden-cliffs-95626.git' }
+  config.action_mailer.default_url_options = { host: 'https://hidden-cliffs-95626.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
